f969a25c5e2a97b6b3368c48ed4f972c24b23f17
Mohamad amin <145129471+itsSheesh@users.noreply.github.com>
2024-03-18 19:26:05 +0330

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import authRoutes from "../backend/routes/auth.routes.js";
+import messageRoutes from "../backend/routes/message.routes.js";
+import usersRoutes from "../backend/routes/users.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server started on port ${PORT}`);
+});
